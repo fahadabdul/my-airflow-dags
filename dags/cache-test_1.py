51e8fe53d1cac915d3f4119dbec4f73323b2ed61
Fahad Abdullah <fahadabdul121@gmail.com>
2026-08-17 13:11:57 +0530

    new dag cache

---
+++ dags/cache-test_1.py
@@ -0,0 +1,21 @@
+# test_cache_import_error.py
+
+import os
+from datetime import datetime
+from airflow.sdk import DAG, task
+
+if os.path.exists("/tmp/break_dag"):
+    raise Exception("Simulated Vault connectivity timeout")
+
+with DAG(
+    dag_id="test_cache_import_error",
+    schedule="*/5 * * * *",
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+) as dag:
+
+    @task
+    def hello():
+        return "hello"
+
+    hello()
